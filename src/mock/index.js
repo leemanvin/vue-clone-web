0bf776d965ac85296c0610cdc4be280a98b3b0d4
leemanvin <82501944+leemanvin@users.noreply.github.com>
2022-09-13 18:15:55 +0800

    mock数据测试

--- src/mock/index.js
+++ src/mock/index.js
@@ -2,5 +2,7 @@ const Mock = require('mockjs');
   
 //格式： Mock.mock( url, post/get , 返回的数据)；
 Mock.mock('/coins/list', 'get', require('./coins/list.json'));
+
+Mock.mock('/index/topbar', 'get', require('./index/topbar.json'));
   
 // Mock.mock('/index/foodlist2', 'post', require('./json/foodlist2.json'));
